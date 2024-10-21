2d55de72e4a98778c647783cecd752ba5c8837a1
Harseerat Sohal <hsohal5@uwo.ca>
2024-10-20 23:19:53 -0400

    formating

--- challenge.rb
+++ challenge.rb
@@ -5,12 +5,12 @@ COMAPNIES_FILE = "companies.json"
 
 # Load JSON files and parse them
 def load_json(file_name)
-	begin
-		file = File.read(file_name)
-		JSON.parse(file, symbolize_names: true)
-	rescue StandardError => e
-		raise "Error reading file #{file_name}: #{e.message}"
-	end
+  begin
+    file = File.read(file_name)
+    JSON.parse(file, symbolize_names: true)
+  rescue StandardError => e
+    raise "Error reading file #{file_name}: #{e.message}"
+  end
 end
 
 def is_valid_user_and_company?(user, company)
@@ -21,73 +21,73 @@ def is_valid_user_and_company?(user, company)
 end
 
 def write_user_info(file, user)
-	file.puts "		#{user[:last_name]}, #{user[:first_name]}, #{user[:email]}"
-	file.puts "		  Previous Token Balance, #{user[:previous_tokens]}"
-	file.puts "		  New Token Balance #{user[:new_tokens]}"
+  file.puts "		#{user[:last_name]}, #{user[:first_name]}, #{user[:email]}"
+  file.puts "		  Previous Token Balance, #{user[:previous_tokens]}"
+  file.puts "		  New Token Balance #{user[:new_tokens]}"
 end
 
 def write_company_info(file, company, users_emailed, users_not_emailed, total_top_up)
-	# Write company info
-	file.puts "	Company Id: #{company[:id]}"
-	file.puts "	Company Name: #{company[:name]}"
+  # Write company info
+  file.puts "	Company Id: #{company[:id]}"
+  file.puts "	Company Name: #{company[:name]}"
 
-	# Write users emailed section
-	file.puts "	Users Emailed:"
-	unless users_emailed.empty?
-		users_emailed.each { |user| write_user_info(file, user) }
-	end
+  # Write users emailed section
+  file.puts "	Users Emailed:"
+  unless users_emailed.empty?
+    users_emailed.each { |user| write_user_info(file, user) }
+  end
 
-	# Write users not emailed section
-	file.puts "	Users Not Emailed:"
-	unless users_not_emailed.empty?
-		users_not_emailed.each { |user| write_user_info(file, user) }
-	end
+  # Write users not emailed section
+  file.puts "	Users Not Emailed:"
+  unless users_not_emailed.empty?
+    users_not_emailed.each { |user| write_user_info(file, user) }
+  end
 
-	# Write total top up for the company
-	file.puts "		Total amount of top ups for #{company[:name]}: #{total_top_up}"
-	file.puts "\n"
+  # Write total top up for the company
+  file.puts "		Total amount of top ups for #{company[:name]}: #{total_top_up}"
+  file.puts "\n"
 end
 
 def generate_output(users, companies)
   File.open('output.txt', 'w') do |file|
-		companies.each do |company|
-			# Select active users who belong to this company
-			company_users = users.select { |user| user[:company_id] == company[:id] && user[:active_status] }
-			
-			next if company_users.empty?
+    companies.each do |company|
+      # Select active users who belong to this company
+      company_users = users.select { |user| user[:company_id] == company[:id] && user[:active_status] }
+      
+      next if company_users.empty?
 
-			users_emailed = []
-			users_not_emailed = []
-			total_top_up = 0
-			company_users.sort_by! { |user| user[:last_name] }
+      users_emailed = []
+      users_not_emailed = []
+      total_top_up = 0
+      company_users.sort_by! { |user| user[:last_name] }
 
-			# Process each user in the company
-			company_users.each do |user|
-				next unless is_valid_user_and_company?(user, company)
+      # Process each user in the company
+      company_users.each do |user|
+        next unless is_valid_user_and_company?(user, company)
 
-				previous_tokens = user[:tokens]
-				user[:tokens] += company[:top_up]
-				total_top_up += company[:top_up]
+        previous_tokens = user[:tokens]
+        user[:tokens] += company[:top_up]
+        total_top_up += company[:top_up]
 
-				user_info = {
-					last_name: user[:last_name],
-					first_name: user[:first_name],
-					email: user[:email],
-					previous_tokens: previous_tokens,
-					new_tokens: user[:tokens]
-				}
+        user_info = {
+          last_name: user[:last_name],
+          first_name: user[:first_name],
+          email: user[:email],
+          previous_tokens: previous_tokens,
+          new_tokens: user[:tokens]
+        }
 
-				if company[:email_status] && user[:email_status]
-					users_emailed << user_info
-				else
-					users_not_emailed << user_info
-				end
-			end
+        if company[:email_status] && user[:email_status]
+          users_emailed << user_info
+        else
+          users_not_emailed << user_info
+        end
+      end
 
-			write_company_info(file, company, users_emailed, users_not_emailed, total_top_up)
-		end
-	end
-	puts 'output.txt file generated successfully.'
+      write_company_info(file, company, users_emailed, users_not_emailed, total_top_up)
+    end
+  end
+  puts 'output.txt file generated successfully.'
 end
 
 # Main method to drive the process
